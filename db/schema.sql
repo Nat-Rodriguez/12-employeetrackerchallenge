108d833c9df75bf1b0dac915994cdefebac05dc1
Nat <n3rodriguez2001@gmail.com>
2024-01-10 17:58:20 -0500

    auto increments for id

--- db/schema.sql
+++ db/schema.sql
@@ -2,12 +2,12 @@ CREATE DATABASE IF NOT EXISTS employee_info_db;
 USE employee_info_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
@@ -15,7 +15,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
